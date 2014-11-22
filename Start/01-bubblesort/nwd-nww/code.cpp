248d1726641a02e42868bb18d3b4845701e87cce
Jakub Czarnowicz <j.czarnowicz@gmail.com>
2014-11-22 16:24:42 +0100

    first class added

---
+++ Start/01-bubblesort/nwd-nww/code.cpp
@@ -0,0 +1,22 @@
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+
+long long NWD(long long a, long long b) {
+	if(a > b) swap(a, b);
+	if(a == 0LL) return b;
+	return NWD(b % a, a);
+}
+
+int main() {
+	int z;
+	scanf("%d", &z);
+	while(z--) {
+		long long a, b;
+		scanf("%lld%lld", &a, &b);
+		long long nwd = NWD(a, b);
+		long long nww = (a * b) / nwd;
+		printf("%lld %lld\n", nwd, nww);
+	}
+	return 0;
+}
