3bf1115eefa1f003a0176fca27a4bcff211488f2
mequrel <meq@meq.com>
2014-11-30 15:19:24 +0100

    4 round problems

---
+++ Start/04-combinatorial/next_permutation/model_solution.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  vector<int> sequence(n);
+
+  for(int i=0; i<n; ++i) {
+    scanf("%d", &sequence[i]);
+  }
+
+  next_permutation(sequence.begin(), sequence.end());
+
+  for(int i=0; i<n; ++i) {
+    printf("%d ", sequence[i]);
+  }
+
+  return 0;
+}
---
+++ Start/04-combinatorial/next_permutation/std_solution.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  vector<int> sequence(n);
+
+  for(int i=0; i<n; ++i) {
+    scanf("%d", &sequence[i]);
+  }
+
+  next_permutation(sequence.begin(), sequence.end());
+
+  for(int i=0; i<n; ++i) {
+    printf("%d ", sequence[i]);
+  }
+
+  return 0;
+}
